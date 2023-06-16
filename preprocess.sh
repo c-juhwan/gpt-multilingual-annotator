79a027d30723299043b489421b79ba0da5744f44
Juhwan Choi <gold32317@gmail.com>
2023-06-16 15:11:11 +0900

    delete old scripts

--- preprocess.sh
+++
@@ -1,6 +0,0 @@
-clear
-
-#python main.py --job=preprocessing --task_dataset=flickr8k
-#python main.py --job=preprocessing --task_dataset=flickr30k
-python main.py --job=preprocessing --task_dataset=coco2014
-#python main.py --job=preprocessing --task_dataset=coco2017
